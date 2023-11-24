d8551741d5a85da83bd70bf47979902345d425da
ismarthawkback <151498795+ismarthawkback@users.noreply.github.com>
2023-11-24 14:31:56 +0000

    Theme Toggle and Theme Context set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import Typography from '@mui/material/Typography'
+import { ThemeContext } from './contexts/ThemeContext'
+import { useContext } from 'react'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 
 
 function App() {
+  const {theme, toggleTheme} = useContext(ThemeContext)
+
+  const appTheme = createTheme({
+    palette: {
+      mode: theme,
+    },
+  });
 
   return (
     <>
-      HI    </>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline />
+      <Typography variant="h1">Hi, {theme}</Typography>
+    </ThemeProvider>
+      
+    </>
   )
 }
 
