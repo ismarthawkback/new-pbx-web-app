54db9fd7d3b514f78f2e57a219ac0c8f66a01905
ismarthawkback <151498795+ismarthawkback@users.noreply.github.com>
2023-11-28 11:47:18 +0000

    Implement Speed dail and selection

--- frontend/src/components/DatabasePage-components/Table/FilterTableExample.jsx
+++ frontend/src/components/DatabasePage-components/Table/FilterTableExample.jsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { IconButton } from '@mui/material';
+import { IconButton, SpeedDial } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
-import { columnGroupsStateInitializer } from '@mui/x-data-grid/internals';
+import AddIcon from "@mui/icons-material/Add";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import Tune from '@mui/icons-material/Tune';
+import Add from '@mui/icons-material/Add';
 
+const actions = [
+  { icon: <AddIcon />, name: "Add", onclick : function () {
+    console.log("Hello World")
+    console.log(selectionModel);
+  } },
+  { icon: <DeleteForeverIcon />, name: "Delete" },
+];
 
 const columns = [
   {
     field: 'edit',
     headerName: 'Edit',
-    width: 100,
+    width: 50,
     sortable: false,
     filterable: false,
     disableColumnMenu: true,
@@ -45,29 +57,49 @@ const generateRows = () => {
 };
 
 const FilterTableExample = () => {
-  const [filterModel, setFilterModel] = React.useState({
-    items: [],
-  });
+  const [selectionModel, setSelectionModel] = React.useState([]);
 
   const rows = React.useMemo(() => generateRows(), []);
 
   
 
   return (
-    <div style={{ height: '60vh', width: '100%' }}>
-    
+    <div style={{ height: "65vh", width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         checkboxSelection
-        filterModel={filterModel}
-        onFilterModelChange={(model) => setFilterModel(model)}
         slots={{
-            toolbar : GridToolbar
+          toolbar: GridToolbar,
         }}
+        disableRowSelectionOnClick
+        rowSelectionModel={selectionModel}
+        onRowSelectionModelChange={setSelectionModel}
       />
-      
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: "absolute", bottom: 16, right: 48 }}
+        icon={<Tune />}
+        direction="left"
+      >
+        {selectionModel.length === 0 ? 
+        <SpeedDialAction
+        icon={<AddIcon />}
+        tooltipTitle={"Add Row"}
+        onClick={(e) => {
+          
+        }}
+        /> :
+        <SpeedDialAction
+        icon={<DeleteForeverIcon />}
+        tooltipTitle={"Delete Rows"}
+        onClick={(e) => {
+          
+        }}
+        />
+      }
+      </SpeedDial>
     </div>
   );
 };
